6a9314c81e0d0675d4e3d292e1df0655bed940f0
Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-11-17 23:18:17 +0300

    Screen Resolution Problem Solved

--- AI_Pacman.py
+++ AI_Pacman.py
@@ -1,17 +1,15 @@
 import sys
 import turtle
 import tkinter as tk
-import numpy as nm
+import random
+import ctypes
+user32 = ctypes.windll.user32
 wn=turtle.Screen()
 wn.bgcolor("black")
 wn.title("Pacman Game With Artificial Intelligence")
-wn.setup(700,700)
-#turtle=turtle.Turtle()
-#turtle.shape("circle")
-#turtle.pen(fillcolor="black", pencolor="yellow", pensize=10)
-#tk.mainloop()
+wn.setup(width=.99,height=.90,startx=0,starty=0)
 
-#maze yarat
+
 class Maze(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -20,50 +18,54 @@ class Maze(turtle.Turtle):
         self.penup()
         self.speed(0)
 
-def create_fruit_or_food():
-    randomVal=nm.random.randomint(601,size=1)
-    print()
+class Fruit_Food(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("circle")
+        self.pen(fillcolor="black", pencolor="yellow", pensize=0)
+        self.penup()
 
+
+#def create_fruit_or_food():
+ #       if random.randint(0,len(grid),random.randint(0,len(grid[0])) not in walls              #20 46
+            
 
 def create_maze(grid):
     for y in range(len(grid)):                      #Labirent İçin Kodlanan Grid'in Tüm Birimleri Çizilecek
         for x in range(len(grid[y])):                
             character = grid[y][x]                   
-            screen_x = -588 + (x * 24)   #Her Bir Square 24x24 Bit       
+            screen_x = -552 + (x * 24)   #Her Bir Square 24x24 Bit       
             screen_y = 288 - (y * 24)    #Ekrandaki Maze Tam Center'lı Gibi Düşünülmeli Analitik Düzlem Üzerinde             
 
             if character == "+":                     
                 maze.goto(screen_x, screen_y)        
                 maze.stamp()   #Elemanın Ekranda Kalmasını Sağlıyor Stampleniyor                      
-                #walls.append((screen_x, screen_y))   
+                walls.append((screen_x, screen_y))  
             
 grid = [
-#Grid İçinde Rastgele Bir Yere Elaman Eklenirken Bunu Grid Üzerinde Boş Olen Yerlerden Birine Yapıcaz.
-"++++++++++++++++++++++++++++++++++++++++", 
-"+P              T+++++++               +",
-"+ ++++++ +++++++ +++++++ +++++++ +++++ +",
-"+                                      +",
-"+ ++++++ ++ +++++++++++++++++ ++ +++++ +",
-"+        ++      +++++++      ++       +",
-"++++++++ +++++++ +++++++ +++++++ +++++++",
-"++++++++ ++                   ++ +++++++",
-"++++++++ ++ ++++++++ ++++++++ ++ +++++++",
-"+                                      +",
-"++++++++ ++ ++++++++ ++++++++ ++ +++++++",
-"++++++++ ++                   ++ +++++++",
-"++++++++ ++ +++++++++++++++++ ++ +++++++",
-"+                +++++++               +",
-"+ ++++++ +++++++ +++++++ +++++++ +++++ +",
-"+     ++                         ++    +",
-"+++++ ++ +++++++ +++++++ +++++++ ++ ++++",
-"+        +++++++    +    +++++++       +",
-"+ +++++++++++++++++ + ++++++++++++++++ +",
-"+                                      +",
-"++++++++++++++++++++++++++++++++++++++++",
+"++++++++++++++++++++++++++++++++++++++++++++++", #Grid İçinde Rastgele Bir Yere Elaman Eklenirken Bunu Grid Üzerinde Boş Olen Yerlerden Birine Yapıcaz.
+"+ s             +                            +", #20*46 =920 Character
+"+  ++++++++++  +++++++++++++  +++++++  ++++  +",
+"+           +                 +        +     +",
+"+  +++++++  +++++++++++++  +++++++++++++++++++",
+"+  +     +  +           +  +                 +",
+"+  +  +  +  +  +  ++++  +  +  +++++++++++++  +",
+"+  +  +  +  +  +  +     +  +  +  +        +  +",
+"+  +  ++++  +  ++++++++++  +  +  ++++  +  +  +",
+"+  +     +  +              +           +  +  +",
+"+  ++++  +  ++++++++++++++++  +++++++++++++  +",
+"+     +  +                    +              +",
+"++++  +  ++++++++++++++++++++++  ++++++++++  +",
+"+  +  +                    +     +     +  +  +",
+"+  +  ++++  +++++++++++++  +  ++++  +  +  +  +",
+"+  +  +     +     +     +  +  +     +     +  +",
+"+  +  +  +++++++  ++++  +  +  +  ++++++++++  +",
+"+                       +  +  +              +",
+"++++  +  +  ++++++++++  +  +  +  +++++++++++++",
+"+++++++++++++++++++++++e++++++++++++++++++++++",
 ]
 maze=Maze()
 walls=[]
-foods=[]
 create_maze(grid)
 
 
